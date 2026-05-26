bec0e48c1ea3cfc7b74b8edeabf66de2f7578fba
HassanAlBarshoumy <HassanaliAlBarshoumy@gmail.com>
2026-05-26 04:26:08 +0300

    Add license file, update buildVars

--- buildVars.py
+++ buildVars.py
@@ -51,9 +51,9 @@ addon_info = {
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel": None,
 	# Add-on license such as GPL 2
-	"addon_license": None,
+	"addon_license": "GPL 2",
 	# URL for the license document the ad-on is licensed under
-	"addon_licenseURL": None,
+	"addon_licenseURL": "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html",
 }
 
 # Define the python files that are the sources of your add-on.
